49b8333156cb914038ab8a2b548663ea058499e9
rickdouglas <rickmiranda23@gmail.com>
2024-05-24 09:43:24 -0300

    feat: accordion card list

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+export function Card({ name, description, price, images }) {
+  return (
+    <div className="w-auto p-3" style={{ margin: '0.5rem', maxWidth: "540px", boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px' }}>
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={images} className="img-fluid rounded" alt="" />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body" style={{ padding: '0', paddingLeft: '1rem' }}>
+            <h5 className="card-title">
+              <button className='btn-primary'>1</button>
+              {name}
+            </h5>
+            <p className="card-text">
+              {description && description.length > 100 ? description.slice(0, 100) + '...' : description}
+            </p>
+            <p className="h6">
+              R${price.toFixed(2)}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  images: PropTypes.string.isRequired,
+};
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,15 @@
 import useFetchMenuDetails from "../hooks/useMenuItems";
+import '../styles/Main.scss';
+import { useState } from 'react';
+import { Card } from "./Card";
 
 export function Main() {
   const { menuDetails, loading } = useFetchMenuDetails();
-  console.log(menuDetails, 'data');
+  const [openAccordion, setOpenAccordion] = useState(null);
+
+  const toggleAccordion = (id) => {
+    setOpenAccordion(openAccordion === id ? null : id);
+  };
 
   return (
     loading ? (
@@ -12,7 +19,6 @@ export function Main() {
     ) : (
       <div className="container">
         <input 
-          style={{ margin: '1rem' }} 
           type="text" 
           className="form-control" 
           id="formGroupExampleInput" 
@@ -22,51 +28,60 @@ export function Main() {
           {menuDetails.sections.map((section) => (
             <div style={{ flex: '0 0 0' }} className="col" key={section.id}>
               {section.images.map((image) => (
-                <img
-                  key={image.id}
-                  src={image.image}
-                  className="rounded-circle"
-                  alt={section.name}
-                  style={{ width: "80px", height: "80px", margin: '1rem' }}
-                />
+                <button className="btn" key={image.id}>
+                  <img
+                    src={image.image}
+                    className="rounded-circle"
+                    alt={section.name}
+                    style={{ width: "80px", height: "80px" }}
+                  />
+                </button>
               ))}
-              <p style={{ paddingLeft: '1.5rem' }} className="lead">{section.name}</p>
+              <p style={{ paddingLeft: '1rem' }} className="h5">{section.name}</p>
             </div>
           ))}
-          {console.log(menuDetails.sections,'sections')}
+          {console.log(menuDetails.sections, 'sections')}
         </div>
-        {menuDetails.sections.map((section) => (
-          <div key={section.id} className="card mb-3" style={{ maxWidth: "100%" }}>
-            <div className="row g-0">
-              <div className="col-md-8">
-                <div className="card-body">
-                  {section.items.map((item) => (
-                    <div key={item.id} className="w-auto p-3" style={{ maxWidth: "540px" }}>
-                      <div className="row g-0">
-                        <div className="col-md-4">
-                          {item.images && item.images.map((image) => (
-                            <img key={image.id} src={image.image} className="img-fluid rounded-start" alt={item.name} />
-                          ))}
-                        </div>
-                        <div className="col-md-8">
-                          <div className="card-body" style={{padding: '0', paddingLeft: '1rem'}}>
-                            <h5 className="card-title">{item.name}</h5>
-                            <p className="card-text">
-                              {item.description && item.description.length > 50 ? item.description.slice(0, 50) + '...' : item.description}
-                            </p>                            
-                            <p className="h6">
-                              R${item.price.toFixed(2)}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
+        <div className="col-8">
+        {menuDetails.sections.map((section, index) => {
+          const isOpen = openAccordion === section.id;
+          return (
+            <div key={section.id} className="accordion accordion-flush" id="accordionFlushExample">
+              <div  className="accordion-item">
+                <h2 className="accordion-header" id={`heading${index}`}>
+                  <button 
+                    className={`accordion-button ${isOpen ? '' : 'collapsed'}`} 
+                    type="button" 
+                    onClick={() => toggleAccordion(section.id)} 
+                    aria-expanded={isOpen} 
+                    aria-controls={`collapse${index}`}
+                  >
+                    {section.name}
+                  </button>
+                </h2>
+                <div 
+                  id={`collapse${index}`} 
+                  className={`accordion-collapse collapse ${isOpen ? 'show' : ''}`} 
+                  aria-labelledby={`heading${index}`} 
+                  data-bs-parent="#accordionFlushExample"
+                >
+                  <div className="accordion-body">
+                    {section.items.map((item) => (
+                      <Card 
+                        key={item.id}
+                        name={item.name}
+                        description={item.description}
+                        price={item.price}
+                        images={item.images && item.images.map((image) => image.image)} 
+                      />
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
+        </div>
       </div>
     )
   );
